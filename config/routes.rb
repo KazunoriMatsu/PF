f0d975d499508053e1057d4962ccb3a3624e3ba2
KazunoriMatsu <niconico.skyhand@gmail.com>
2019-12-10 21:02:33 +0900

    route1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  # customer用のルーティング
+  devise_for :customers, :controllers => {
+  :registrations => 'customers/registrations',
+  :sessions => 'customers/sessions'
+	}
+
+	# root to: 'customers/sign_in'
 end
