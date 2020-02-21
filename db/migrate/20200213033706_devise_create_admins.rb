587b273a5d6b4b4eaa942dae740ba918ca43dbc4
KazunoriMatsu <niconico.skyhand@gmail.com>
2020-02-21 13:32:53 +0900

    fixed

--- db/migrate/20200213033706_devise_create_admins.rb
+++ db/migrate/20200213033706_devise_create_admins.rb
@@ -6,6 +6,14 @@ class DeviseCreateAdmins < ActiveRecord::Migration[5.2]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :last_name, null: false
+      t.string :first_name, null: false
+      t.string :last_name_kana, null: false
+      t.string :first_name_kana, null: false
+      t.string :phone_number1, null: false
+      t.string :phone_number2, null: false
+      t.string :phone_number3, null: false
+      t.datetime :deleted_at
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,14 @@ ActiveRecord::Schema.define(version: 2020_02_13_033706) do
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "phone_number1", null: false
+    t.string "phone_number2", null: false
+    t.string "phone_number3", null: false
+    t.datetime "deleted_at"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
