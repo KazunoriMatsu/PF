1add3cc685b306179cee9fdc23ce216a5c3265c4
KazunoriMatsu <niconico.skyhand@gmail.com>
2020-03-17 21:30:09 +0900

    fixed sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
 	# before_action :authenticate_user!
-	# before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
     root_path # ログイン後に遷移するpathを設定
@@ -10,5 +10,12 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     root_path # ログアウト後に遷移するpathを設定
   end
+
+  protected
+	def configure_permitted_parameters
+	  devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name,
+	  	:last_name_kana, :first_name_kana, :phone_number1, :phone_number2, :phone_number3, :email])
+	end
+
 
 end
